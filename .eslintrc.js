efd6b8fd4157a1b0dea1a79d43452b29e2c44a3f
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2023-07-02 07:35:14 +0000

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,31 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true
-//   },
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
 
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:react/recommended',
-//     'standard'
-//   ],
-//   // eslint-disable-next-line no-dupe-keys
-//   extends: [
-//     'plugin:react/recommended',
-//     'standard'
-//   ],
-//   parser: 'babel-eslint',
-//   parserOptions: {
-//     ecmaFeatures: {
-//       jsx: true
-//     },
-//     ecmaVersion: 12,
-//     sourceType: 'module'
-//   },
-//   plugins: [
-//     'react'
-//   ],
-//   rules: {
-//     'react/jsx-uses-react': 'error',
-//     'react/jsx-uses-vars': 'error',
-//     'react/prop-types': 0,
-//     'no-tabs': 0,
-//     'no-mixed-spaces-and-tabs': [0, 'smart-tabs']
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 0,
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': [0, 'smart-tabs']
 
-//   }
-// }
+  }
+}
